ac17f1dd8275eeec94919a00ec7f336e57513ee7
Na'im Muhammad <naimqm@gmail.com>
2023-12-01 19:27:59 -0600

    Create base logic for GUI navigation.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Krystian Szczech, Na'im Muhammad, & Michael Yelich - Fall 2023
+# Main file for GUI interface and data handling
+
 from modules import geoip
 import json
-import PySimpleGUI as sg
+import PySimpleGUI as sg # GUI library
 
 # Functions relating to data handling
 # First position is data type (string), second position is a list of json strings
@@ -45,3 +48,47 @@ def dumpCSV(fileName, keyList):
     with open(fileName, "w") as outFile:
         outFile.write(output)
         outFile.close()
+
+# Functions relating to GUI
+    # Element layout
+def makeWindow():
+    layout = [[sg.Button("Spam Identifier"), sg.Button("Disposable"), sg.Button("File"), sg.Button("GeoIP")]]
+    window = sg.Window('IP Address/Domain Name Lookup', layout)
+    return window
+
+window = makeWindow()
+    # Event loop for window processing
+while True:
+    event, values = window.read()
+    
+    # Spam Identifier
+    if event == "Spam Identifier":
+        layout_spam = [[sg.Text("Spam Identifier")], [sg.Text("Enter an email address to check for spam: "), sg.InputText()], 
+                       [sg.Button("Ok"), sg.Button("New Request"), sg.Button("Cancel")]]
+        window = sg.Window('Spam Identifier', layout_spam)
+
+    # Disposable
+    if event == "Disposable":
+        layout = [  [sg.Text('Geo IP'), sg.Text('Please Enter IP adresss'), sg.InputText()],
+                    [sg.Button("Ok"), sg.Button("New Request"), sg.Button("Cancel")]]
+        window = sg.Window('Disposable', layout)
+    
+    # File
+    if event == "File":
+        layout = [  [sg.Text('File'), sg.Text(''), sg.InputText()], # ? File Path maybe
+                    [sg.Button("Ok"), sg.Button("New Request"), sg.Button("Cancel")]]
+        window = sg.Window('File', layout)
+    
+    # Disposable
+    if event == "GeoIP":
+        layout = [  [sg.Text('Geo IP'), sg.Text('Please Enter IP adresss'), sg.InputText()],
+                    [sg.Button("Ok"), sg.Button("New Request"), sg.Button("Cancel")]]
+        window = sg.Window('Geo IP', layout)
+    
+    
+    if event == "New Request":
+        window = makeWindow()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    
+window.close()
